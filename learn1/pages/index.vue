0b0263e169ab9acea3aee7aaf1738e7e580f4def
guser <enclis@gmail.com>
2022-12-16 00:44:08 +0000

    update

---
+++ learn1/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ClientOnly>
+      <!-- ... -->
+      <template #fallback>
+        <!-- this will be rendered on server side -->
+        <p>Loading comments...</p>
+      </template>
+   </ClientOnly> 
+  </div>
+</template>
+
+<script setup> 
+useHead({  title: 'My App'})
+</script>
