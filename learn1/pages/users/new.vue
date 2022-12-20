01835ec9fc7eac0554207fb06270091702f1f3e5
guser <enclis@gmail.com>
2022-12-20 16:26:03 +0000

    update

--- learn1/pages/users/new.vue
+++ learn1/pages/users/new.vue
@@ -1,47 +1,3 @@
-<script setup>
-
-    const first_name = ref('')
-    const email = ref('')
-    const mobile = ref('')
-    const password = ref('')
-    const city = ref('')
-    const country = ref('')
-
-    const register = async () => {
-
-        const resData =  {
-            first_name: first_name.value,
-            email: email.value,
-            mobile: mobile.value,
-            password: password.value, 
-            city: city.value, 
-            country: country.value
-        }
-
-        const url = 'https://3000-guser-base-x6edhj2hepj.ws-us79.gitpod.io/users'
-
-        console.log(resData);
-
-        try {
-            const { data, error } = await useFetch(url, {
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json",
-                },
-                method: 'POST',
-                mode: 'no-cors',
-                body: resData,
-            });
-        } catch (error) {
-        
-            console.log(error)
-        }
-
-    
-    }
-
-</script>
-
 <template>
     <p>
         Register
@@ -52,15 +8,47 @@
             <input v-model="first_name" placeholder="First Name" />
             <input v-model="email" placeholder="Email" />
             <input v-model="mobile" placeholder="Mobile Number" />
-            <input v-model="password" type="password"  placeholder="Password" />
+            <input v-model="password" type="password" placeholder="Password" />
 
             <br/>
             <input v-model="city" placeholder="City" />
             <input v-model="country" placeholder="Country" />
             
-            <input type="submit" value="Submit" @click.prevent="register"/>
+            <input type="submit" value="Submit" @click.prevent="createUser"/>
         </form>
     </div>
 
 </template>
 
+<script setup >
+
+    const first_name = ref('');
+    const email = ref('');
+    const mobile = ref('');
+    const password = ref('');
+    const city = ref('');
+    const country = ref('');
+
+    async function createUser() {
+
+        const URL = "https://3000-guser-base-x6edhj2hepj.ws-us79.gitpod.io/users";
+        
+        const form = {
+            first_name: first_name.value,
+            email: email.value,
+            mobile: mobile.value,
+            password: password.value, 
+            city: city.value, 
+            country: country.value,
+        };
+
+        const options = {
+            method: "POST",
+            mode: 'no-cors',
+            body: JSON.stringify(form),
+
+        };
+
+        const response = await fetch(URL, options );
+    }
+</script>
